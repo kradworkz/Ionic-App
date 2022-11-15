57bedb16c10bdda5b62c6c241b09f95f642ceec9
kradworkz <80160702+kradworkz@users.noreply.github.com>
2022-11-15 10:15:53 +0800

    wew

--- src/app/recipe/recipe.page.ts
+++ src/app/recipe/recipe.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe',
@@ -13,17 +14,29 @@ export class RecipePage implements OnInit {
   item: Object;
 
   constructor(
-    private activatedroute: ActivatedRoute
+    private activatedroute: ActivatedRoute,
+    private loadingCtrl: LoadingController
   ) { }
   
   
   ngOnInit() {
+    this.showLoading();
     this.id = this.activatedroute.snapshot.paramMap.get("id");  
 
     fetch('./assets/json/recipe.json').then(res => res.json()).then(json => {
       this.recipes = json;
       this.item = this.recipes.find(item => item.id == this.id);
     })  
+  }
+
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      duration: 1000,
+      spinner: 'circles',
+    });
+
+    loading.present();
   }
 
 }
